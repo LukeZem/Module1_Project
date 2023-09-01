0d23c44dcffd20716ab945f96aa882f99c10bf06
LukeZem <lucaszemlin@outlook.com>
2023-09-01 14:37:52 -0700

    created more bot functionality, local storage functionality, flash cards now work

--- flash_cards/flashCard_script.js
+++ flash_cards/flashCard_script.js
@@ -1,5 +1,7 @@
 const inputElement = document.querySelector("#userInput");
 const cards = document.querySelectorAll(".card");
+const chatPage = document.querySelector(".chat-page");
+const clearCardsButton = document.getElementById("clearCardsButton");
 
 let userName = "User";
 const topicChoice = ["psychology", "Cybersecurity", "Computer Science"];
@@ -18,7 +20,8 @@ const botResponses = [
 	["I am just a bot", "I am a bot. What are you?"], //3
 	["define self aware..."], //4
 ];
-const botMemory = {};
+const botDefaults = [];
+const botMemory = [[], []];
 
 //flipping the flash card when clicked
 let prevCardIndex; //keep track of previous card so it can be unflipped when next card is clicked
@@ -31,30 +34,56 @@ cards.forEach((card, index) => {
 
 //
 inputElement.addEventListener("keydown", (e) => {
-	// console.log(e);
 	if (e.code === "Enter") {
-		// console.log("Enter pressed");
 		let input = inputElement.value;
 		inputElement.value = "";
 		output(input);
-		// console.log(output(input));
 	}
 });
 
 const output = (input) => {
-	let text = input.toLowerCase().replace(/[^\w\s\d]/gi, "");
+	let term = "";
+	let definition = "";
+	//adding the term for the front of the card
+	//Regex searchs for "add" then aggregates everything that follows
 
-	text = text
-		.replace(/ a /g, " ")
-		.replace(/whats/g, "What is")
-		.replace(/please /g, "")
-		.replace(/ please/g, "");
-	//need to add more delimiter regex...
+	//term adding
+	const termRegex = /add\s+(.+)/gi;
+	const termInput = input.replace(termRegex, (match, capturedText) => {
+		console.log("Captured Text:", capturedText);
+		term = capturedText;
+		console.log("Captured Text:", term);
+	});
+	if (input.includes("term")) {
+		addToFrontCard(term);
+	}
 
-	// implementation of chatbot response...
+	//definition adding
+	const definitionRegex = /add\s+(.+)/gi;
+	const definitionInput = input.replace(definitionRegex, (match, capturedText) => {
+		console.log("Captured Text:", capturedText);
+		definition = capturedText;
+		console.log("Captured Text:", definition);
+	});
+	if (input.includes("definition")) {
+		addToBackCard(definition);
+	}
 
-	console.log("return of compare function: ", compare(userInputs, botResponses, text));
-	//testing regex output
+	//if user just wants to talk to the BOT
+	if (!input.includes(":")) {
+		let text = input.toLowerCase().replace(/[^\w\s\d]/gi, "");
+		text = text
+			.replace(/ a /g, " ")
+			.replace(/whats/g, "What is")
+			.replace(/please /g, "")
+			.replace(/ please/g, "");
+		//need to add more delimiter regex...
+
+		// implementation of chatbot response...
+		addToChatBox(text, compare(userInputs, botResponses, text));
+
+		//bot memory
+	}
 };
 
 /**
@@ -77,6 +106,67 @@ const compare = (userInputs, botResponses, string) => {
 	return response || `Sorry I don't understand`; // default message if no match found..
 };
 
-/**function to change flash card front/back content based on selection
- * This function will likely have to be called inside the output function
+const addToChatBox = (input, response) => {
+	const messagesDiv = document.querySelector("#messages");
+	messagesDiv.innerHTML = `<span class="response">${response}</span>`;
+	// <span class="userText">${input}</span>
+	// <span class="response">${response}</span>
+	// `;
+};
+
+/**functions to change flash card front/back content based on selection
+ * These functions will likely have to be called inside the output function
  */
+let cardCount = 0;
+let frontSet = false;
+let backSet = false;
+const addToFrontCard = (term) => {
+	const card = document.getElementById(cardCount);
+	card.children[0].textContent = term;
+	localStorage.setItem(`card${cardCount}_term`, term);
+	frontSet = true;
+	if (backSet === true) {
+		cardCount++;
+		frontSet = false;
+	}
+};
+const addToBackCard = (definition) => {
+	const card = document.getElementById(cardCount);
+	card.children[1].textContent = definition;
+	localStorage.setItem(`card${cardCount}_definition`, definition);
+	backSet = true;
+	if (frontSet === true) {
+		cardCount++;
+		backSet = false;
+	}
+};
+
+// Function to populate cards from local storage
+const populateCardsFromLocalStorage = () => {
+	for (let i = 0; i < cards.length; i++) {
+		const term = localStorage.getItem(`card${i}_term`);
+		const definition = localStorage.getItem(`card${i}_definition`);
+
+		if (term && definition) {
+			const card = document.getElementById(i);
+			card.children[0].textContent = term;
+			card.children[1].textContent = definition;
+		}
+	}
+};
+
+// Call the function to populate cards when the page is loaded
+window.addEventListener("load", populateCardsFromLocalStorage);
+
+clearCardsButton.addEventListener("click", () => {
+	// Loop through all cards and remove their data from local storage
+	for (let i = 0; i < cards.length; i++) {
+		localStorage.removeItem(`card${i}_term`);
+		localStorage.removeItem(`card${i}_definition`);
+
+		// Optionally, you can reset the card content to its default state
+		const card = document.getElementById(i);
+		card.children[0].textContent = "Front Content";
+		card.children[1].textContent = "Back Content";
+	}
+});
